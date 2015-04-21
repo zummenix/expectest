aab644909f70db0cf882f7b66410f9b387bf685a
Aleksey Kuznetsov <zummenix@gmail.com>
2015-04-21 22:31:31 +0500

    Add tests for 'be_none' matcher.

--- tests/option.rs
+++ tests/option.rs
@@ -15,3 +15,14 @@ fn some_should_panic() {
     expect!(None::<u8>).to(be_some());
     expect!(None::<u8>).to(be_some().value(9));
 }
+
+#[test]
+fn none() {
+    expect!(None::<u8>).to(be_none());
+}
+
+#[test]
+#[should_panic]
+fn none_should_panic() {
+    expect!(Some(4)).to(be_none());
+}

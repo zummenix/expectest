a7e18b29678b9e2c5a3119b569d133a283183a9d
Aleksey Kuznetsov <zummenix@gmail.com>
2015-04-27 22:21:02 +0500

    Add more external tests for 'be_equal_to' matcher.

--- tests/partial_eq.rs
+++ tests/partial_eq.rs
@@ -4,8 +4,23 @@ extern crate expectest;
 use expectest::prelude::*;
 
 #[test]
-fn expect_macro() {
-    expect!(1 + 1).to(be_equal_to(2));
+fn equality_of_vecs() {
     expect!(vec![1, 2, 3]).to(be_equal_to([1, 2, 3]));
+}
+
+#[test]
+fn equality_of_strings() {
     expect!("hello".to_string()).to(be_equal_to("hello"));
+}
+
+#[test]
+#[should_panic]
+fn equality_of_vecs_should_panic() {
+    expect!(vec![1, 2, 2]).to(be_equal_to([1, 2, 3]));
+}
+
+#[test]
+#[should_panic]
+fn equality_of_strings_should_panic() {
+    expect!("hell0".to_string()).to(be_equal_to("hello"));
 }

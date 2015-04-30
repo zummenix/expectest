d1d3d8283233871deafff9236a77aec782374a3a
Aleksey Kuznetsov <zummenix@gmail.com>
2015-04-30 21:02:24 +0500

    Add 'delta' method to 'close_to' matcher.

--- src/matchers/floats.rs
+++ src/matchers/floats.rs
@@ -15,6 +15,13 @@ pub fn close_to<E>(expected: E) -> CloseTo<E> where E: Float {
     }
 }
 
+impl<E> CloseTo<E> {
+    pub fn delta(mut self, v: E) -> CloseTo<E> {
+        self.delta = v;
+        self
+    }
+}
+
 impl<E> Matcher<E, E> for CloseTo<E>
     where
         E: Float + fmt::Debug {
@@ -59,6 +66,28 @@ mod tests {
     #[should_panic]
     fn big_zero_matches_zero_should_panic() {
         assert!(close_to(0.0011_f32).matches(&0.0));
+    }
+
+    #[test]
+    fn zero_delta_matches_zero() {
+        assert!(close_to(0.0).delta(0.1).matches(&0.0_f32));
+    }
+
+    #[test]
+    fn small_zero_delta_matches_zero() {
+        assert!(close_to(0.1_f32).delta(0.1).matches(&0.0));
+    }
+
+    #[test]
+    fn close_to_one_delta_failure_message() {
+        let message = close_to(1.0_f32).delta(0.1).failure_message("to", &0.0);
+        assert!(message == "expected to be close to <1> ±0.1, got <0>");
+    }
+
+    #[test]
+    #[should_panic]
+    fn big_zero_delta_matches_zero_should_panic() {
+        assert!(close_to(0.11_f32).delta(0.1).matches(&0.0));
     }
 
     #[test]
--- src/matchers/mod.rs
+++ src/matchers/mod.rs
@@ -2,7 +2,7 @@
 pub use self::partial_eq::{ be_equal_to };
 pub use self::bool::{ be_true, be_false };
 pub use self::option::{ be_some, BeSome, be_none, BeNone };
-pub use self::floats::close_to;
+pub use self::floats::{ close_to, CloseTo };
 
 mod partial_eq;
 mod bool;

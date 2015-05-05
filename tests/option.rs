c7be6e20956c3e23e60655e3665c814550bbc5e7
Aleksey Kuznetsov <zummenix@gmail.com>
2015-05-05 20:43:35 +0500

    Split external tests for 'option' matcher.

--- tests/option.rs
+++ tests/option.rs
@@ -4,25 +4,34 @@ extern crate expectest;
 use expectest::prelude::*;
 
 #[test]
-fn some() {
+fn to_be_some() {
     expect!(Some(6)).to(be_some());
+}
+
+#[test]
+fn to_be_some_value() {
     expect!(Some(9)).to(be_some().value(9));
 }
 
 #[test]
 #[should_panic]
-fn some_should_panic() {
+fn to_be_some_should_panic() {
     expect!(None::<u8>).to(be_some());
+}
+
+#[test]
+#[should_panic]
+fn to_be_some_value_should_panic() {
     expect!(None::<u8>).to(be_some().value(9));
 }
 
 #[test]
-fn none() {
+fn to_be_none() {
     expect!(None::<u8>).to(be_none());
 }
 
 #[test]
 #[should_panic]
-fn none_should_panic() {
+fn to_be_none_should_panic() {
     expect!(Some(4)).to(be_none());
 }

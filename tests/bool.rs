6d4f3ac4a0ff884c6175cc994495f33574fb30ed
Aleksey Kuznetsov <zummenix@gmail.com>
2015-05-05 20:38:54 +0500

    Split up externals tests for 'bool' matchers.

--- tests/bool.rs
+++ tests/bool.rs
@@ -4,13 +4,33 @@ extern crate expectest;
 use expectest::prelude::*;
 
 #[test]
-fn bool() {
+fn to_be_true() {
     expect!(9 == 9).to(be_true());
+}
+
+#[test]
+fn to_be_false() {
     expect!(9 == 6).to(be_false());
 }
 
 #[test]
-fn bool_negation() {
-    expect!(9 == 9).to_not(be_false());
+fn to_be_true_negation() {
     expect!(9 == 6).not_to(be_true());
+}
+
+#[test]
+fn to_be_false_negation() {
+    expect!(9 == 9).to_not(be_false());
+}
+
+#[test]
+#[should_panic]
+fn to_be_true_should_panic() {
+    expect!(0 == 1).to(be_true());
+}
+
+#[test]
+#[should_panic]
+fn to_be_false_should_panic() {
+    expect!(0 == 0).to(be_false());
 }

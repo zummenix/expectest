2f7ce43c41d1c1347f228a021c48acbc4bb28015
Aleksey Kuznetsov <zummenix@gmail.com>
2015-09-01 21:58:41 +0500

    Clean up tests for 'result' module.

--- src/matchers/result.rs
+++ src/matchers/result.rs
@@ -71,7 +71,7 @@ impl<A, E, T> Matcher<Result<T, A>, E> for BeErr<E>
 #[cfg(test)]
 mod tests {
     use super::{be_ok, be_err};
-    use core::Matcher;
+    use core::{Matcher, Join};
 
     fn ok_result(value: u32) -> Result<u32, &'static str> {
         Ok(value)
@@ -82,58 +82,14 @@ mod tests {
     }
 
     #[test]
-    fn be_ok_matches_ok() {
-        assert!(be_ok().matches(&ok_result(5)));
+    fn be_ok_failure_message() {
+        let m = be_ok().failure_message(Join::To, &err_result("error"));
+        assert_eq!(m, "expected to be Ok, got <Err(\"error\")>");
     }
 
     #[test]
-    fn be_ok_value_matches_ok() {
-        assert!(be_ok().value(1).matches(&ok_result(1)));
-    }
-
-    #[test]
-    #[should_panic]
-    fn be_ok_matches_err_should_panic() {
-        assert!(be_ok().matches(&err_result("error")));
-    }
-
-    #[test]
-    #[should_panic]
-    fn be_ok_value_matches_ok_should_panic() {
-        assert!(be_ok().value(1).matches(&ok_result(2)));
-    }
-
-    #[test]
-    #[should_panic]
-    fn be_ok_value_matches_err_should_panic() {
-        assert!(be_ok().value(1).matches(&err_result("error")));
-    }
-
-    #[test]
-    fn be_err_matches_err() {
-        assert!(be_err::<&'static str>().matches(&err_result("error")));
-    }
-
-    #[test]
-    fn be_err_value_matches_err() {
-        assert!(be_err().value("error").matches(&err_result("error")));
-    }
-
-    #[test]
-    #[should_panic]
-    fn be_err_matches_ok_should_panic() {
-        assert!(be_err::<&'static str>().matches(&ok_result(1)));
-    }
-
-    #[test]
-    #[should_panic]
-    fn be_err_value_matches_err_should_panic() {
-        assert!(be_err().value("error").matches(&err_result("error!")));
-    }
-
-    #[test]
-    #[should_panic]
-    fn be_err_value_matches_ok_should_panic() {
-        assert!(be_err().value("error").matches(&ok_result(1)));
+    fn be_err_failure_message() {
+        let m = be_err::<&str>().failure_message(Join::To, &ok_result(2));
+        assert_eq!(m, "expected to be Err, got <Ok(2)>");
     }
 }
